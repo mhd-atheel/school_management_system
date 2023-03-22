f1f0f746b0588389c11067fb43ac03aacea78513
aadhil <mhd-atheel>
2023-03-22 08:14:14 +0530

    adding the firebase details

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:school_management_system/screens/splachScreen.dart';
@@ -15,8 +16,19 @@ void main() async{
             projectId: "school-management-system-bff13"
         )
     );
-  }else{
+  }else if(Platform.isAndroid){
     await Firebase.initializeApp();
+  }else{
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyDBJpVk2KA22YAuw5Z66R2xSookXpBfCJE",
+            appId: "1:295074751396:ios:4fc2413777c1d1b35ed731",
+            messagingSenderId: "295074751396",
+            projectId: "school-management-system-bff13",
+            authDomain: "school-management-system-bff13.firebaseapp.com",
+            storageBucket: "school-management-system-bff13.appspot.com",
+        )
+    );
   }
   runApp(const MyApp());
 }
